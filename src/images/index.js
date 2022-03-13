158dae4da9e693154f1a2816527ceaa1ce917d07
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-03-13 22:59:43 +0100

    change directory name photos -> images

---
+++ src/images/index.js
@@ -0,0 +1,8 @@
+import cat from './cat.jpg';
+import dog from './dog.jpg';
+import eagle from './eagle.jpg';
+import horse from './horse.jpg';
+import panda from './panda.jpg';
+import tiger from './tiger.jpg';
+
+export default { cat, dog, eagle, horse, panda, tiger };
