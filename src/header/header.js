755a19f814ef01623f2285b97678595b0054bb50
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-03-24 21:11:22 +0100

    add header

---
+++ src/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <div className='logo'>Guess the word!</div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.css';
 import { TextArea } from './textArea/TextArea.js';
+import { Header } from './header/header';
 
+ReactDOM.render(<Header></Header>, document.getElementById('header'));
 ReactDOM.render(<TextArea />, document.getElementById('text-area'));
