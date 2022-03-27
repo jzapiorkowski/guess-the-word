0bcf2005787419085514e61697761cb65fd12510
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-03-27 23:52:12 +0200

    create layout for difficulty change in sidebar

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sidebar.css';
+import { VscTriangleDown, VscTriangleUp } from 'react-icons/vsc';
 
 export const Sidebar = () => {
-  return <nav className='sidebar'></nav>;
+  const [isHidden, setIsHidden] = useState(true);
+
+  const toggleDifficulty = () => {
+    setIsHidden(!isHidden);
+  };
+
+  return (
+    <nav className='sidebar'>
+      <div className='difficulty-change'>
+        <div className='choose-difficulty' onClick={toggleDifficulty}>
+          <p>Choose difficulty</p>
+          {isHidden ? <VscTriangleDown /> : <VscTriangleUp />}
+        </div>
+        {!isHidden ? (
+          <ul>
+            <li>Easy</li>
+            <li>Hard</li>
+          </ul>
+        ) : null}
+      </div>
+    </nav>
+  );
 };
