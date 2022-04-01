b5da063a93997e150b41f88b395205099a3b4545
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-01 17:36:53 +0200

    Keyboard component code refactor

--- src/keyboard/Keyboard.js
+++ src/keyboard/Keyboard.js
@@ -11,40 +11,31 @@ export const Keyboard = (props) => {
     props.handleBackspace();
   };
 
+  const letters = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
+  ];
+
+  const buttons = letters.map((row, rowIndex) => {
+    return (
+      <div key={rowIndex}>
+        {row.map((letter, buttonIndex) => {
+          return (
+            <Button
+              key={buttonIndex}
+              changetext={handleChange}
+              letter={letter}
+            ></Button>
+          );
+        })}
+      </div>
+    );
+  });
+
   return (
     <div className='keyboard'>
-      <div className='first-row'>
-        <Button changeText={handleChange} letter='Q'></Button>
-        <Button changeText={handleChange} letter='W'></Button>
-        <Button changeText={handleChange} letter='E'></Button>
-        <Button changeText={handleChange} letter='R'></Button>
-        <Button changeText={handleChange} letter='T'></Button>
-        <Button changeText={handleChange} letter='Y'></Button>
-        <Button changeText={handleChange} letter='U'></Button>
-        <Button changeText={handleChange} letter='I'></Button>
-        <Button changeText={handleChange} letter='O'></Button>
-        <Button changeText={handleChange} letter='P'></Button>
-      </div>
-      <div className='second-row'>
-        <Button changeText={handleChange} letter='A'></Button>
-        <Button changeText={handleChange} letter='S'></Button>
-        <Button changeText={handleChange} letter='D'></Button>
-        <Button changeText={handleChange} letter='F'></Button>
-        <Button changeText={handleChange} letter='G'></Button>
-        <Button changeText={handleChange} letter='H'></Button>
-        <Button changeText={handleChange} letter='J'></Button>
-        <Button changeText={handleChange} letter='K'></Button>
-        <Button changeText={handleChange} letter='L'></Button>
-      </div>
-      <div className='third-row'>
-        <Button changeText={handleChange} letter='Z'></Button>
-        <Button changeText={handleChange} letter='X'></Button>
-        <Button changeText={handleChange} letter='C'></Button>
-        <Button changeText={handleChange} letter='V'></Button>
-        <Button changeText={handleChange} letter='B'></Button>
-        <Button changeText={handleChange} letter='N'></Button>
-        <Button changeText={handleChange} letter='M'></Button>
-      </div>
+      {buttons}
       <div className='fourth-row'>
         <Backspace handleBackspace={handleBackspace}></Backspace>
       </div>
