89448f2757090fc0b06c5ac10a6bebeb08e05168
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-01 13:56:38 +0200

    fix letterTiles size for mobile devices

--- src/letterTiles/LetterTiles.js
+++ src/letterTiles/LetterTiles.js
@@ -5,10 +5,31 @@ export const LetterTiles = (props) => {
   const [letterTiles, setLetterTiles] = useState(() => {
     const letters = [];
     for (let i = 0; i < props.images[props.currentImage].length; i++) {
-      letters.push(<div key={i} className='letter-tile'></div>);
+      letters.push(<div key={i} className='letter-tile' style={size()}></div>);
     }
     return letters;
   });
+
+  function size() {
+    if (window.innerWidth < 800) {
+      return {
+        height: `calc((100vw - 10px) / 9 )`,
+        width: `calc((100vw - 10px) / 9 )`,
+      };
+    } else if (window.innerWidth < 1200) {
+      return {
+        height: `calc((100vw - 10px) / 9 )`,
+        width: `calc((100vw - 10px) / 9 )`,
+      };
+    }
+  }
+
+  const tileColor = (index) => {
+    return props.images[props.currentImage][index].toUpperCase() ===
+      props.word[index].toUpperCase()
+      ? { backgroundColor: '#59C9A5' }
+      : { backgroundColor: '#C95D63' };
+  };
 
   useEffect(() => {
     setLetterTiles(() => {
@@ -17,11 +38,11 @@ export const LetterTiles = (props) => {
         for (let i = 0; i < props.images[props.currentImage].length; i++) {
           letters.push(
             props.word[i] !== undefined ? (
-              <div key={i} className='letter-tile'>
+              <div key={i} className='letter-tile' style={size()}>
                 {props.word[i]}
               </div>
             ) : (
-              <div key={i} className='letter-tile'></div>
+              <div key={i} className='letter-tile' style={size()}></div>
             )
           );
         }
@@ -34,16 +55,13 @@ export const LetterTiles = (props) => {
                 key={i}
                 className='letter-tile'
                 style={
-                  props.images[props.currentImage][i].toUpperCase() ===
-                  props.word[i].toUpperCase()
-                    ? { backgroundColor: '#59C9A5' }
-                    : { backgroundColor: '#C95D63' }
+                  size() ? Object.assign(size(), tileColor(i)) : tileColor(i)
                 }
               >
                 {props.word[i]}
               </div>
             ) : (
-              <div key={i} className='letter-tile'></div>
+              <div key={i} className='letter-tile' style={size()}></div>
             )
           );
         }
