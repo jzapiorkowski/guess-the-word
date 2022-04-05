f0cca432213fd025c89b5478e603a9aa79ba8b1b
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-05 22:27:54 +0200

    move all elements and body selectors' styles to html file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/styles.css';
 import { App } from './app/App.js';
 
 ReactDOM.render(<App />, document.getElementById('app'));
