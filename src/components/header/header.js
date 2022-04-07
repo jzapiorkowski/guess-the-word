c20d5a9785e2879fd3ec5798162de7d8eb67b3e8
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-07 17:02:18 +0200

    change toggleSidebar from useState to useReducer

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,11 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Sidebar } from '../sidebar/Sidebar';
 import './header.css';
 import { GoThreeBars } from 'react-icons/go';
 
 export const Header = (props) => {
-  const [sidebar, setSidebar] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebar(!sidebar);
-  };
+  // const [sidebar, setSidebar] = useState(false);
+  const [sidebar, toggleSidebar] = useReducer((sidebar) => !sidebar, false);
 
   return (
     <div className='header'>
